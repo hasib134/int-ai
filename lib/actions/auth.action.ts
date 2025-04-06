4d01dc62424a0d06a27bc22034b142a8a341a45a
hasib134 <113293466+hasib134@users.noreply.github.com>
2025-04-06 14:33:56 +0530

    msg

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,35 @@
+'use server';
+import { db } from "@/firebase/admin";
+// import { cookies } from "next/headers";
+
+
+export async function sugnUp(params:SignUpParams) {
+    const {uid,name,email} = params;
+    try{
+        const userRecord = await db.collection('users').doc(uid).get();
+
+        if(userRecord.exists){
+            return{
+                success:false,
+                message : 'User already exists.Please sign in instead'
+            }
+        }
+
+        await db.collection('users').doc(uid).set({
+            name,email
+        })
+    }
+    catch(err:any){
+        console.log("Error creating a user",err);
+        if(err.code==='auth/email-already-exists'){
+            return{
+                success:false,
+                message:'This email already in use' 
+            }
+        }
+        return{
+            success:false,
+            message :"Failed to create an account"
+        }
+    }
+}
